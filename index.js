8e3b743d67c08b296f9759cca2c9cd1c15ed1631
Shubhang Tripathi <t-shtri@microsoft.com>
2019-07-09 15:55:16 -0700

    Changes to the code

--- index.js
+++ index.js
@@ -2,75 +2,110 @@ const parse = require('csv-parse/lib/sync')
 const fs = require('fs');
 const Parser = require('simple-text-parser')
 const textParser = new Parser();
+const createCsvWriter = require('csv-writer');
 
 const inputFilePath = 'C:\\Shubhang\\Work\\asoms-validations-scraper\\Story170_details.csv'
-
-const input = fs.readFileSync('Story170_details.csv');
+const outputFilePath = 'output.csv'
+//const input = fs.readFileSync('Story170_details.csv');
+const input = fs.readFileSync('Story170_1.csv');
 const records = parse(input, {
-    columns: false,
-    skip_empty_lines: true
-  })
+  columns: false,
+  skip_empty_lines: true
+})
 
 const warnings = [];
 const errorArray = [];
-const validation = function(validationObj){
+const validationCollections = [];
+const validation = function (validationObj) {
   this.Text = validation;
   this.Type;
   return this
 }
-const parseValidation = function(text){
-   
-    // start with errors and warnings for now
-    //console.log(text)
-    var validationTextArray = text.split('<br>');
 
-    var indexOfErrors = validationTextArray.indexOf('Errors:')
-    var indexOfWarnings = validationTextArray.indexOf('Warnings:')
-  console.log('outside. Index of Errors: '+ indexOfErrors + " Index of Warnings: "+indexOfWarnings)
-    if (indexOfErrors >= 0 && indexOfWarnings >= 0) {
-      console.log('inside')
-      if (indexOfErrors < indexOfWarnings) {
-        var counter = 1;
-      while(counter < (indexOfWarnings - indexOfErrors))
-      {
-        var validationObj = new validation();
-        validationObj.Text = validationTextArray[counter];
-        validationObj.Type = 'Error'
-        errorArray.push(validationObj)
-        counter++;
-      }
-      console.log("Error array has: "+counter -1 +" elements")
-      if(counter = indexOfWarnings){
-        counter++
-        while(counter < validationTextArray.length){
-          var validationObj = new validation();
-        validationObj.Text = validationTextArray[counter];
-        validationObj.Type = 'Warning'
-        warnings.push(validationObj)
-        counter++;
-        }
-        console.log("Warnings array has: "+counter -1 +" elements")
-      }
-      }
-      
+const csvWriter = createCsvWriter({
+  path: 'output.csv',
+  header: [
+      {id: 'details', title: 'Details'},
+      {id: 'type', title: 'Type'}
+  ]
+});
+const addValidationToCollection = function (validationText, validationType) {
+  var validationObj = new validation();
+  validationObj.Text = validationText;
+  validationObj.Type = validationType;
+  validationCollections.push(validationObj);
+  csvWriter.writeRecords(validationObj).then(console.log("done"));
+}
+
+const determineValidationCategory = function (validationTextArray, lowerIndex, higherIndex, lowIndexName, highIndexName) {
+  
+  validationTextArray.forEach(function (element) {
+    
+    if (validationTextArray.indexOf(element) > lowerIndex && validationTextArray.indexOf(element) < higherIndex) {
+
+      console.log("case1");
+      console.log("Validation params: element= "+element+" element index: "+ validationTextArray.indexOf(element) +" lowerIndex: "+lowerIndex+" higher index: "+higherIndex+" lowIndexName: "+lowIndexName+" higher index name: "+highIndexName)
+      addValidationToCollection(element, lowIndexName)
     }
-
-    textParser.addRule('/Errors:/', function(warningText){
-        console.log(warningText)
-        warnings.push(warningText)
-    })
-
-  };
-  records.forEach(record => {
-      parseValidation(record[0])
+    else if (validationTextArray.indexOf(element) > higherIndex) {
+      console.log("case2");
+      console.log("Validation params: element= "+element+" element index: "+ validationTextArray.indexOf(element) +" lowerIndex: "+lowerIndex+" higher index: "+higherIndex+" lowIndexName: "+lowIndexName+" higher index name: "+highIndexName)
+      addValidationToCollection(element, highIndexName)
+    }
   });
+}
+const parseValidation = function (text) {
+  var validationTextArray = text.split('<br>');
+  var indexOfErrors = validationTextArray.indexOf('Errors:')
+  var indexOfWarnings = validationTextArray.indexOf('Warnings:')
 
-errorArray.forEach(function(errorObj){
-  console.log(errorObj);
+  if (indexOfErrors < indexOfWarnings) {
+    
+    determineValidationCategory(validationTextArray, indexOfErrors, indexOfWarnings, 'Error', 'Warning');
+  }
+  else if (indexOfErrors > indexOfWarnings) {
+  
+    determineValidationCategory(validationTextArray, indexOfWarnings, indexOfErrors, 'Warning', 'Error');
+
+  }
+  else if (indexOfErrors < 0 && indexOfWarnings < 0) {
+    console.log("case3");
+    validationTextArray.forEach(function (element) {
+      addValidationToCollection(element, "Unknown")
+    });
+  }
+}
+Array.prototype.unique = function() {
+  return this.filter(function (value, index, self) { 
+    return self.indexOf(value) === index;
+  });
+}
+records.forEach(record => {
+  parseValidation(record[0])
+});
+
+validationCollections.forEach(function(element){
+  if (!fs.existsSync(finalPathFile))
+  writer = csvWriter({ headers: ["header1", "header2"]});
+else
+  writer = csvWriter({sendHeaders: false});
+
+writer.pipe(fs.createWriteStream(finalPathFile, {flags: 'a'}));
+writer.write({
+  header1:"hello",
+  header2:"world",
+});
+writer.end();
 })
-// warnings.forEach(function(warning){
-//   console.log(warning)
-// });
+
+
+
+
+
+
+
+
+
 
 
 
